0c4190bf5a5903b71780b4a99d5aaa3425a813f9
TravisJon <mhdyoga1028@gmail.com>
2023-12-10 22:41:09 +0700

    Progress Enemy
    lanjutkan bg... bagaimana cara ketika dia mengejar kita atau melihat kita game over

--- Assets/Script/WaypointFollower.cs
+++ Assets/Script/WaypointFollower.cs
@@ -1,31 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
 {
-    [SerializeField] private GameObject[] waypoint;
-    private int currentWaypointIndex = 0;
-
-    [SerializeField] private float speed = 2f;
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = waypoint2.transform;
+        anim.SetBool("isWalk", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(waypoint[currentWaypointIndex].transform.position, transform.position) < .1f)
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == waypoint2.transform)
         {
-            currentWaypointIndex++;
-            if (currentWaypointIndex >= waypoint.Length)
-            {
-                currentWaypointIndex = 0;
-            }
+            rb.velocity = new Vector2(speed, 0);
         }
-        transform.position = Vector2.MoveTowards(transform.position, waypoint[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == waypoint2.transform)
+        {
+            flip();
+            currentPoint = waypoint1.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == waypoint1.transform)
+        {
+            flip();
+            currentPoint = waypoint2.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localscale = transform.localScale;
+        localscale.x *= -1;
+        transform.localScale = localscale;  
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(waypoint1.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(waypoint2.transform.position, 0.5f);
+        Gizmos.DrawLine(waypoint1.transform.position, waypoint2.transform.position);
     }
 }
