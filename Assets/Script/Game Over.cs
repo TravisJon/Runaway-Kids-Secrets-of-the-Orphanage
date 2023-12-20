01c55ef7326c56543db29f43b5563a058fdc68f5
TravisJon <mhdyoga1028@gmail.com>
2023-12-20 12:26:27 +0700

    Done Game Over

--- Assets/Script/Game Over.cs
+++ Assets/Script/Game Over.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
+     // Start is called before the first frame update
     public WaypointFollower enemy;
-    public float gameOverDistance = 1.0f;
+    public float gameOverDistance = 2.0f;
     public string gameOverSceneName = "GameOverScene"; // Change this to your game over scene name
     public GameObject gameOverScreen;
+    public float gameOverScreenDuration = 2.0f;
+
+    private bool isGameOver = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (IsGameOver())
+        if (!isGameOver && IsGameOver())
         {
-            HandleGameOver();
+            isGameOver = true;
+            StartCoroutine(ShowGameOverScreen());
         }
     }
 
@@ -29,11 +34,23 @@ public class GameOver : MonoBehaviour
         return false;
     }
 
-    void HandleGameOver()
+    void CheckAndEndTimer()
     {
+        if (TimeController.instance != null)
+        {
+            TimeController.instance.EndTimer();
+        }
+    }
+
+    IEnumerator ShowGameOverScreen()
+    {
+        CheckAndEndTimer();
         gameOverScreen.SetActive(true);
-        // You can add additional game over logic here, such as showing a game over screen, playing a sound, etc.
-        // For now, let's just reload the specified scene.
-        SceneManager.LoadScene("Scene 1");
+
+        // Wait for the specified duration
+        yield return new WaitForSeconds(gameOverScreenDuration);
+
+        // Reload the specified scene
+        SceneManager.LoadScene(gameOverSceneName);
     }
 }
